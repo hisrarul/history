4d780b803b267da0837b2b44f486e7cb2a8ba9e1
root <root@ip-172-31-35-159.ap-south-1.compute.internal>
2020-05-15 02:19:33 +0000

    added ansible

---
+++ ansible/ansible.sh
@@ -0,0 +1,7 @@
+
+## Install ansible on ubuntu
+sudo apt update
+apt install software-properties-common
+apt-add-repository --yes --update ppa:ansible/ansible
+apt install ansible
+ansible --version
