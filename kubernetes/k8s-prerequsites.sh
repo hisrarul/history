31fb8f70a6d36de24b0f2e49a81bc2e106c3aba8
root <root@ip-172-31-43-72.ap-south-1.compute.internal>
2020-05-25 00:23:41 +0000

    added pre-requesite file

---
+++ kubernetes/k8s-prerequsites.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-get install docker.io
+systemctl daemon-reload
+systemctl restart kubelet
