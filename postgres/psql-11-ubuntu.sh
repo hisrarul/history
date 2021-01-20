69a250748d0d12a4d76e006669b130fe04c72d81
hisrarul <35391097+hisrarul@users.noreply.github.com>
2021-01-20 16:53:38 +0530

    Create psql-11-ubuntu.sh

---
+++ postgres/psql-11-ubuntu.sh
@@ -0,0 +1,6 @@
+echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" >>  /etc/apt/sources.list.d/pgdg.list
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+sudo apt-get install wget ca-certificates
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install postgresql-client
