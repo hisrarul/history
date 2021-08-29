efd956999832418707dae84dc32d6f085785276a
Israrul <hisrarul@gmail.com>
2021-08-30 00:03:37 +0530

    nodejs day 7

---
+++ node-js/enhanced-version/models/product.js
@@ -0,0 +1,77 @@
+const products = [];
+
+const path = require('path')
+const fs = require('fs')
+
+module.exports = class Product {
+    constructor(titl, imageUrl, description, price) {
+        this.title = titl;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
+    }
+    save() {
+        const p = path.join(
+          path.dirname(process.mainModule.filename),
+          'data',
+          'products.json'
+        );
+        fs.readFile(p, (err, fileContent) => {
+          let products = [];
+          if (!err) {
+            products = JSON.parse(fileContent);
+          }
+          products.push(this);
+          fs.writeFile(p, JSON.stringify(products), err => {
+            console.log(err);
+          });
+        });
+      }
+    // save() {
+    //     // products.push(this);
+    //     const p = path.join(
+    //         path.dirname(process.mainModule.filename),
+    //         'data',
+    //         'products.json'
+    //     );
+    //     console.log(p)
+    //     fs.readFile(p, (err, fileContent) => {
+    //         if (!err) {
+            
+    //             products = JSON.parse(fileContent);
+    //         }
+    //         products.push(this)
+    //         fs.writeFile(p, JSON.stringify(products), (err) => {
+    //             console.log(err);
+    //         });
+    //     });
+    // }
+
+    static fetchAll(cb) {
+        const p = path.join(
+          path.dirname(process.mainModule.filename),
+          'data',
+          'products.json'
+        );
+        fs.readFile(p, (err, fileContent) => {
+          if (err) {
+            cb([]);
+          }
+          cb(JSON.parse(fileContent));
+        });
+      }
+
+    // static fetchAll(cb) {
+    //     const p = path.join(
+    //         path.dirname(process.mainModule.filename),
+    //         'data',
+    //         'products.json'
+    //     );
+    //     fs.readFile(p, (err, fileContent) => {
+    //         if (err) {
+    //             cb([]);
+    //         }
+    //         cb(JSON.parse(fileContent));
+    //     });
+    // }
+};
---
+++ node-js/enhanced-version/routes/shop.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const express = require('express');
+
+const shopController = require('../controllers/shop');
+
+const router = express.Router();
+
+router.get('/', shopController.getIndex);
+router.get('/products', shopController.getProducts);
+router.get('/cart', shopController.getCard);
+router.get('/orders', shopController.getOrders);
+router.get('/checkout', shopController.getCheckout);
+
+module.exports = router;
